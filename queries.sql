1d12bad7469d809db455c5fc6b9715f7218f42d7
TVAnpilogova <155022698+TVAnpilogova@users.noreply.github.com>
2024-01-08 01:10:50 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -62,3 +62,53 @@ order by
 name,
 weekday
 ;
+
+Первый отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age >= 41 THEN '40+'
+    END AS age_category, -- возрастная группа
+    COUNT(*) AS count -- количество человек в группе
+FROM
+    customers
+GROUP BY
+    age_category
+ORDER BY
+    age_category;
+
+Во втором отчете предоставьте данные по количеству уникальных покупателей и выручке, которую они принесли
+
+select
+TO_CHAR(sale_date, 'YYYY-MM') as date, --дата в указанном формате
+COUNT (distinct customer_id) as total_customers, --количество покупателей
+round(sum(distinct s.quantity*p.price),0) as income --принесенная выручка
+from sales as s
+join products as p on 
+p.product_id = s.product_id
+group by date
+order by date asc
+;
+
+Третий отчет следует составить о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0)
+
+select
+CONCAT(c.first_name,' ',c.last_name) as customer, --имя и фамилия покупателя
+TO_CHAR(MIN(s.sale_date), 'YYYY-MM-DD') as sale_date, --дата покупки
+CONCAT(e.first_name,' ',e.last_name) as seller --имя и фамилия продавца
+from customers as c
+join sales as s on
+s.customer_id = c.customer_id
+join employees as e on
+s.sales_person_id = e.employee_id
+join products as p on 
+p.product_id = s.product_id
+where
+p.price = 0
+group by c.customer_id,
+e.employee_id
+order by c.customer_id
+;
+
+
