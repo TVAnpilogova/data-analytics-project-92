26adb24c88e325da72d6e195c27a52cca96b9359
TVAnpilogova <155022698+TVAnpilogova@users.noreply.github.com>
2024-01-14 18:04:28 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -82,8 +82,8 @@ ORDER BY
 
 select
 TO_CHAR(sale_date, 'YYYY-MM') as date, --дата в указанном формате
-COUNT (distinct customer_id) as total_customers, --количество покупателей
-round(sum(s.quantity*p.price),0) as income --принесенная выручка
+COUNT(distinct customer_id) as total_customers, --количество покупателей
+floor(sum(s.quantity*p.price)) as income --принесенная выручка
 from sales as s
 join products as p on 
 p.product_id = s.product_id
